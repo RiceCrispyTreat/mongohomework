31307d7e8d3a1e28de19396199640a4ce86579d7
Scott Anderson <frostlocke@gmail.com>
2019-09-21 21:36:05 -0600

    got server running

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let express = require ('express');
-
+let mongoose = require('mongoose');
+let expressHandlebars = require('express-handlebars');
 let PORT = process.env.PORT || 3000;
 
 let app = express();
@@ -8,7 +9,27 @@ let router = express.Router();
 
 app.use(express.static(__dirname + '/public'));
 
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.use(router);
+
+let db = process.env.MONGO_URL || 'mongodb://localhost/mongoHeadlines',
+
+mongoose.connect(db, function(error){
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log('mongoose connection is successful');
+    }
+})
 
 app.listen(3000, function() {
     console.log("App running on port 3000!");
